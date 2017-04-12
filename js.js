6d70ef4088badbe3bf0d3b9722a08c0f3a7aaaa4
warizz <warizz.yutanan@gmail.com>
2017-04-12 13:20:56 +0700

    big update with cats!

--- js.js
+++ js.js
@@ -1,6 +1,34 @@
 (() => {
-  const footer_year = new Date().getFullYear();
-  document.getElementById('footer-year').innerHTML = footer_year;
+  function copyToClipboard(element_id) {
+    const textarea = document.createElement('textarea');
+    textarea.textContent = document.getElementById(element_id).innerText;
+    textarea.style.position = 'fixed';
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alert('copied');
+  }
+
+  function fetchCat() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+        if (xmlhttp.status == 200) {
+          const res = JSON.parse(xmlhttp.response);
+          document.getElementById('img-cat-gif').src = res.data.image_url;
+        } else if (xmlhttp.status == 400) {
+          // alert('There was an error 400');
+        } else {
+          // alert('something else other than 200 was returned');
+        }
+      }
+    };
+
+    xmlhttp.open("GET", 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat', true);
+    xmlhttp.send();
+  }
 
   if ('serviceWorker' in navigator) {
     window
@@ -12,4 +40,16 @@
           .catch((err) => console.log('ServiceWorker registration failed: ', err));
       });
   }
+
+  document
+    .querySelector('button[name="btn-copy-phone-number"]')
+    .addEventListener('click', () => copyToClipboard('txt-phone-number'))
+
+  document
+    .querySelector('button[name="btn-copy-mail"]')
+    .addEventListener('click', () => copyToClipboard('txt-mail'))
+
+  document.getElementById('footer-year').innerHTML = new Date().getFullYear();
+
+  fetchCat();
 })();
