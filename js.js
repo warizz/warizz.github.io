28d12d7a5d322e38f48df3bef68a052a48a045b7
warizz <warizz.yutanan@gmail.com>
2017-04-12 13:24:24 +0700

    change giphy api to https

--- js.js
+++ js.js
@@ -26,7 +26,7 @@
       }
     };
 
-    xmlhttp.open("GET", 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat', true);
+    xmlhttp.open("GET", 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat', true);
     xmlhttp.send();
   }
 
