ba1b62afcf8bd05025109cd99bfc56a7cff9852f
warizz <warizz.yutanan@gmail.com>
2021-03-07 08:58:24 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8b81eb3f58445220e900.js"
+    "url": "webpack-runtime-e4618302523114be405a.js"
   },
   {
     "url": "app.84bacfb137219a7aef25.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "239f40c7542e9c918853b5479cb351c8"
+    "revision": "f754842b568367d760327b4b62f98ad0"
   },
   {
     "url": "component---src-pages-404-js-cd0e892337332606eab5.js"
